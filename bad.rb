33f399668615e1da54c0b7421785cb656dda9961
Balmung421 <zcook421@gmail.com>
2017-08-16 13:21:25 -0600

    Passing default state and greeting tests

--- bad.rb
+++ bad.rb
@@ -1,8 +1,16 @@
 class BadConnection
 
+
   def initialize
+
+  end
+
+  def ready_to_quit
     ready_to_quit = false
   end
 
+  def greeting
+   "HELLO, THIS IS A GROCERY STORE!"
+  end
 
 end
--- bad_test.rb
+++ bad_test.rb
@@ -14,5 +14,14 @@ class BadConnectionTest < Minitest::Test
     assert_instance_of BadConnection, @bad
   end
 
+  def test_for_default_quit_state
+    # refute bad.ready_to_quit
+    assert_equal false, bad.ready_to_quit
+  end
+
+  def test_for_greeting
+    greeting = "HELLO, THIS IS A GROCERY STORE!"
+    assert_equal "HELLO, THIS IS A GROCERY STORE!", bad.greeting
+  end
 
 end
